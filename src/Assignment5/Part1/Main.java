e15c73d48e74b95940e8566ed6a4b8a7e2c37085
Chen Yue <yuechen049@gmail.com>
2023-12-21 16:39:52 -0500

    added testing of Pair class in Main class of assignment5 part1

--- src/Assignment5/Part1/Main.java
+++ src/Assignment5/Part1/Main.java
@@ -18,5 +18,31 @@ public class Main {
         wordList.remove(3);
         System.out.println("Contains the word \"orange\": " + wordList.contains("orange"));
         System.out.println(wordList);
+
+        // Qst 3: Generic Class
+        Pair<Integer,String> pair = new Pair<>(3,"hi");
+        System.out.println(pair);
+
+        // Qst 4: Using Generic Class
+        Pair<Double, Double> pair2 = new Pair<>(2.3, 4.5);
+        System.out.println(pair2);
+
+        // Qst 5: ArrayList with Generics
+        ArrayList<Pair<?,?>> arr = new ArrayList<>();
+        arr.add(pair);
+        arr.add(pair2);
+        System.out.println(arr);
+
+        // Qst 6: Challenge
+        printArrayList(wordList);
+        printArrayList(arr);
+    }
+
+    // Qst 6: Challenge
+    public static <E> void printArrayList(ArrayList<E> arrayList){
+        System.out.println("Printing ArrayList: ");
+        for(E e : arrayList){
+            System.out.println("\t" + e);
+        }
     }
 }
