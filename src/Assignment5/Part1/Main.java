a6d04ecaf895f798a9d2ecd3b1789492f4fdd1ab
Chen Yue <yuechen049@gmail.com>
2023-12-21 16:33:23 -0500

    added arraylist operations for assignment5 part1

--- src/Assignment5/Part1/Main.java
+++ src/Assignment5/Part1/Main.java
@@ -1,4 +1,22 @@
 package Assignment5.Part1;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        // Qst 1: ArrayList Operations
+        ArrayList<String> wordList = new ArrayList<String>();
+        wordList.add("apple");
+        wordList.add("banana");
+        wordList.add("orange");
+        wordList.add("grape");
+        wordList.add("kiwi");
+        System.out.println(wordList);
+
+        // Qst 2: ArrayList Manipulation:
+        wordList.addFirst("pear");
+        wordList.remove(3);
+        System.out.println("Contains the word \"orange\": " + wordList.contains("orange"));
+        System.out.println(wordList);
+    }
 }
